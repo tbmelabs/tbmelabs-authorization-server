411c683b0831607e5da3f669f5559524d5c981ff
bbortt <timon.borter@post.ch>
2018-10-14 21:50:43 +0200

    stable version including translation.

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -6,6 +6,14 @@ import {AppComponent} from './app.component';
 import {UiModule} from './ui/ui.module';
 
 import {TitleService} from './services/routing/title.service';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -14,6 +22,13 @@ import {TitleService} from './services/routing/title.service';
   imports: [
     BrowserModule.withServerTransition({appId: 'tbme-labs-authorization-server'}),
     AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     UiModule
   ],
   providers: [TitleService],
