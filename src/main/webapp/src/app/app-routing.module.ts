881fa89d4acfff33b3a68935772a689c661fe2ce
bbortt <timon.borter@post.ch>
2018-10-14 11:51:16 +0200

    outsource home.

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from './app.component';
+import {HomeComponent} from './ui/home/home.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent, data: {title: 'Welcome to TBME Labs'}},
   {path: '**', redirectTo: '/'}
 ];
 
--- src/main/webapp/src/app/ui/ui.module.ts
+++ src/main/webapp/src/app/ui/ui.module.ts
@@ -3,12 +3,13 @@ import {CommonModule} from '@angular/common';
 import {LayoutComponent} from './layout/layout.component';
 import {HeaderComponent} from './layout/header/header.component';
 import {FooterComponent} from './layout/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, HomeComponent],
   exports: [LayoutComponent]
 })
 export class UiModule {
